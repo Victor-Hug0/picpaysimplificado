2a83cc12f0d7641566356c02aeac258c0a97536b
Victor Hugo Moura Barreto <victorhugomourabarreto1@gmail.com>
2024-07-21 11:48:30 -0300

    feat: update UserService

--- src/main/java/com/picpaysimplificado/service/UserService.java
+++ src/main/java/com/picpaysimplificado/service/UserService.java
@@ -2,11 +2,13 @@ package com.picpaysimplificado.service;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dto.UserDTO;
 import com.picpaysimplificado.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -27,7 +29,16 @@ public class UserService {
         return userRepository.findById(id).orElseThrow(() -> new Exception("Usuário não encontrado!"));
     }
 
-    public void saveUser (User user) {
+    public void saveUser(User user){
         userRepository.save(user);
+    }
+
+    public User createUser(UserDTO data){
+        User user = new User(data);
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
